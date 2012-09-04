758f496df0915b6ad7098f76ae2148f1c2d3242b
Robert Richter <robert.c.richter@gmail.com>
2012-09-04 17:40:57 -0500

    Added a normalization function & nested iterations
     - Values are now read from a file again instead of STDIN
     - Values are normalized upon being read
     - The weight-update loops is now inside of an "iteration" loop.
        One "iteration" is one presentation of the entire set of data,
        and the learning curve is supposed to be the sum of squared
        error after each one of these iterations. So, now there is the
        weight-update loop, which reads through the entire file once,
        being repeated a number of times. The sum of squared error is
        printed after each one of these iterations.
    
     - My sum of squared error now seems to converge!!! :D

--- main.c
+++ main.c
@@ -2,9 +2,14 @@
 #include <stdlib.h>
 #include <string.h>
 
+#define INPUT_FILE "/home/Bobby/git/adaline-intro/learning-set.txt"
+
 /* GLOBAL VARIABLES START */
 
-const float LEARNING_RATE = 0.000000001;
+const float LEARNING_RATE = 0.5;
+const float NORMALIZERS[] = {540.0, 359.4, 174.7, 288.0, 32.2, 1145.0, 992.6, 365.0, 82.6};
+
+FILE* input_file;
 
 /* GLOBAL VARIABLES END */
 
@@ -18,11 +23,14 @@ float* get_datapoint(){
   
   /* first eight values are input, ninth is 1 (bias), tenth is desired value */
   
-  if(scanf("%f\t%f\t%f\t%f\t%f\t%f\t%f\t%f\t%f \n",
+  if(fscanf(input_file, "%f\t%f\t%f\t%f\t%f\t%f\t%f\t%f\t%f \n",
   &x[0], &x[1], &x[2], &x[3], &x[4], &x[5], &x[6], &x[7], &x[8]) != 9){
     x[0] = -1;
   }
   else {
+    for(int i = 0; i < 9; i++){
+      x[i] /= NORMALIZERS[i];
+    }
     x[9] = x[8];
     x[8] = 1;
   }
@@ -51,29 +59,76 @@ void weight_update(float x[], float w[]){
   }
 }
 
+void learn(){
+  int iterations = 1;
+  
+  float w[9] = {1, 1, 1, 1, 1, 1, 1, 1, 1}; // 9th weight is bias
+  
+  
+  for(iterations = 1; iterations <= 500; iterations ++){
+  
+    float sumsqr_error = 0;
+    float sumsqr = 0;
+    int count = 1;
+    fseek(input_file, 0, SEEK_SET);
+    float* x = get_datapoint();
+    
+    while(x[0] != -1){
+      sumsqr += calc_error(x, w) * calc_error(x, w);
+      sumsqr_error = sumsqr / (2 * count);
+      //printf("%i:\t%f\n", count, sumsqr_error);
+      weight_update(x, w);
+      x = get_datapoint();
+      count++;
+    }
+    
+    printf("After iteration #%i:\t%f\n", iterations, sumsqr_error);
+  }
+  printf("Final:\t%f\t%f\t%f\t%f\t%f\t%f\t%f\t%f\t%f \n",
+     w[0], w[1], w[2], w[3], w[4], w[5], w[6], w[7], w[8]);
+  
+  return;
+}
+
+void test(){
+  
+  float w[9] = {0.313159, 0.301605,  0.916192, 0.513296, 1.003430,
+                  -0.209391 , 0.012439, 0.074814, 0.998135}; // 9th weight is bias
+  float* x = get_datapoint();
+  
+  
+  printf("Testing with:\t%f\t%f\t%f\t%f\t%f\t%f\t%f\t%f\t%f \n",
+     w[0], w[1], w[2], w[3], w[4], w[5], w[6], w[7], w[8]);
+  
+  
+  
+  float sumsqr_error = 0;
+  int count = 1;
+  fseek(stdin, SEEK_SET, 0);
+  
+  while(x[0] != -1){
+    sumsqr_error += calc_error(x, w) * calc_error(x, w);
+    x = get_datapoint();
+    count++;
+  }
+    
+  sumsqr_error /= 2 * count;
+    
+  printf("Error on test set:\t%f\n", sumsqr_error);
+  
+  return;
+}
+
+
 /* FUNCTION DEFINITIONS END */
 
 /* MAIN START */
 
 int main(){
   
-  int count = 1;
+  input_file = fopen(INPUT_FILE, "r");
   
-  float w[9] = {1, 1, 1, 1, 1, 1, 1, 1, 1}; // 9th weight is bias
-  float* x = get_datapoint();
-  
-  
-  while(x[0] != -1){
-    printf("%i:\t%f\t%f\t%f\t%f\t%f\t%f\t%f\t%f\t%f \n",
-     count, w[0], w[1], w[2], w[3], w[4], w[5], w[6], w[7], w[8]);
-  
-    weight_update(x, w);
-    x = get_datapoint();
-    count++;
-  }
-  
-  printf("Final:\t%f\t%f\t%f\t%f\t%f\t%f\t%f\t%f\t%f \n",
-     w[0], w[1], w[2], w[3], w[4], w[5], w[6], w[7], w[8]);
+  learn();
   
   return 0;
 }
