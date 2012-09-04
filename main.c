77831474ac3fc7a907b36b96bcdc3334e19937b9
Robert Richter <robert.c.richter@gmail.com>
2012-09-03 21:10:18 -0500

    Arguments now control output
    Invoking the program with "curve" outputs learning curve data,
    invoking with "weights" outputs the final weights.

--- main.c
+++ main.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <sys/types.h>
+#include <string.h>
 
 /* TYPE DEFINITIONS START */
 
@@ -71,23 +71,33 @@ void weight_update(datapoint data, weights_type &weights){
 
 /* MAIN START */
 
-int main(){
+int main(int argc, char* argv[]){
   
   int count = 1;
   float total_error = 0;
   float step_error = 0;
   
+  char print_curve = 0;
+  char print_weights = 0;
+  
   weights_type w = {0, 0, 0, 0, 0, 0, 0, 0};
   datapoint data = get_datapoint();
   
+  if((strcmp(argv[1], "curve") == 0) || (strcmp(argv[2], "curve") == 0))
+    print_curve = 1;
+  if((strcmp(argv[1], "weights") == 0) || (strcmp(argv[2], "weights") == 0))
+    print_weights = 1;
+  
   
   while(data.x[0] != -1){
-    step_error = calc_error(data, w);
+    if(print_curve == 1){
+      step_error = calc_error(data, w);
     
-    total_error += step_error * step_error;
-    total_error /= 2 * count;
+      total_error += step_error * step_error;
+      total_error /= 2 * count;
     
-    printf("%i\t%f\n", count, total_error);
+      printf("%i\t%f\n", count, total_error);
+    }
     
     weight_update(data, w);
     
@@ -95,8 +105,10 @@ int main(){
     count++;
   }
   
-  printf("Final Weights:\n%f\t%f\t%f\t%f\t%f\t%f\t%f\t%f\t \n",
+  if(print_weights == 1){
+    printf("%f\t%f\t%f\t%f\t%f\t%f\t%f\t%f\t \n",
       w.w[0], w.w[1], w.w[2], w.w[3], w.w[4], w.w[5], w.w[6], w.w[7]);
+  }
   
   return 0;
 }
